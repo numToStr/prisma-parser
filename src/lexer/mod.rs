124c86a96dff41af8f8f2891ef2eb64a9ed55e5c
numToStr <vikasraj1911@gmail.com>
2022-03-28 15:02:47 +0530

    explicit tokens

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -4,7 +4,7 @@ pub use token::*;
 use std::ops::Range;
 
 use chumsky::{
-    prelude::{filter, just, one_of, skip_then_retry_until, take_until, Simple},
+    prelude::{filter, just, skip_then_retry_until, take_until, Simple},
     text::{self, TextParser},
     Parser,
 };
@@ -26,14 +26,22 @@ impl Lexer {
     }
 
     fn lex() -> impl Parser<char, Vec<Span>, Error = Err> {
-        // A parser for @@
-        let attr = just("@@").to(Token::Attr);
-
-        // A parser for operators
-        let operator = one_of(".=?@").map(Token::Op);
-
-        // A parser for control characters (delimiters, semicolons, etc.)
-        let ctrl = one_of("()[]{}:,").map(Token::Ctrl);
+        // parsers for operators
+        let attr = just("@@")
+            .to(Token::Attr)
+            .or(just('@').to(Token::Prop))
+            .or(just('=').to(Token::Assign))
+            .or(just('?').to(Token::Optional))
+            .or(just('.').to(Token::Dot))
+            // parsers for control characters (delimiters, semicolons, etc.)
+            .or(just('(').to(Token::LeftParen))
+            .or(just(')').to(Token::RightParen))
+            .or(just('[').to(Token::LeftSquare))
+            .or(just(']').to(Token::RightSquare))
+            .or(just('{').to(Token::LeftCurly))
+            .or(just('}').to(Token::RightCurly))
+            .or(just(':').to(Token::Colon))
+            .or(just(',').to(Token::Comma));
 
         // A parser for numbers
         let num = text::int(10)
@@ -60,18 +68,16 @@ impl Lexer {
             "Int" => Token::Int,
             "DateTime" => Token::DateTime,
             // everything else is user defined
-            _ => Token::Ident(ident),
+            _ => Token::Id(ident),
         });
 
         // A parser for linewise comment
         let comment = just("//").then(take_until(just('\n'))).padded();
 
         // A single token can be one of the above
-        let token = ctrl
+        let token = attr
             .or(string)
             .or(num)
-            .or(attr) // NOTE: attr should come before operator
-            .or(operator)
             .or(ident)
             .recover_with(skip_then_retry_until([]));
 
--- src/lexer/token.rs
+++ src/lexer/token.rs
@@ -1,16 +1,32 @@
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub enum Token {
     // Keywords
-    DataSource,    // datasource
-    Generator,     // client
-    Model,         // model
-    Enum,          // enum
-    Ctrl(char),    // (){}()
-    Op(char),      // =?@
-    Ident(String), // like source/generator/model name
-    Str(String),   // string value
-    Num(String),   // number value
-    Attr,          // @@
+    DataSource, // datasource
+    Generator,  // client
+    Model,      // model
+    Enum,       // enum
+
+    // Ctrl chars
+    LeftParen,   // (
+    RightParen,  // )
+    LeftCurly,   // {
+    RightCurly,  // }
+    LeftSquare,  // [
+    RightSquare, // ]
+    Colon,       // :
+    Comma,       // ,
+
+    // Operators
+    Attr,     // @@
+    Prop,     // @
+    Assign,   // =
+    Optional, // ?
+    Dot,      // .
+
+    // User defined
+    Id(String), // user defined identified (like source/generator/model name, table fields)
+    Str(String), // string value
+    Num(String), // number value
 
     // Data Types
     DateTime,
